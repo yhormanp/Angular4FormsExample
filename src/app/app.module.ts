ed117b9afefad3fec1aa2d515872eb4a523148f7
yperez <yperez.gap@gmail.com>
2018-01-03 16:12:40 -0500

    until page 175, demo form sku builder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component'
 import { AppComponent } from './app.component';
+import {
+  FormsModule,
+  ReactiveFormsModule
+  } from '@angular/forms';
+
+  import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // <-- add this
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
